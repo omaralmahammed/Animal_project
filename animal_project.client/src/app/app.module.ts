e7e7d59b6cb9939e7b789f0116f0ed011d6ad128
Hadeel-Alzoubi <hadeelzoubi12@gmail.com>
2024-10-21 17:04:26 +0300

    اللهم لا حول ولا قوة الا بك

--- animal_project.client/src/app/app.module.ts
+++ animal_project.client/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  // <-- Import FormsModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Hadeel/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
