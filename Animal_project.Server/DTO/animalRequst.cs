85ea6df2c9db793be6f3af6a96423bfde102343f
Raneem-Alhalahlah <raneemalhalahleh@gmail.com>
2024-10-21 17:10:40 +0300

    g

---
+++ Animal_project.Server/DTO/animalRequst.cs
@@ -0,0 +1,9 @@
+﻿namespace Animal_project.Server.DTO
+{
+    public class animalRequst
+    {
+        public string Name { get; set; } = null!;
+        public IFormFile? Image1 { get; set; }
+
+    }
+}
