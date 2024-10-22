c89b39a7e5a3d68af57553946325fd7c361edbbe
SuhaKrasneh <suha.kras967@gmail.com>
2024-10-22 16:42:17 +0300

    nav edited

--- animal_project.client/src/app/Admin/get-by-story-id/get-by-story-id.component.ts
+++ animal_project.client/src/app/Admin/get-by-story-id/get-by-story-id.component.ts
@@ -7,29 +7,5 @@ import { SuhaService } from '../../Suha/suha.service';
   styleUrl: './get-by-story-id.component.css'
 })
 export class GetByStoryIdComponent {
-  //posts: Post[] = [];
-  //errorMessage: string | null = null;
-
-  //constructor(private postService: SuhaService) { }
-
-  //ngOnInit(): void {
-  //  const storyId = 1; // Replace with the actual story ID you want to fetch
-  //  this.getPosts(storyId);
-  //}
-
-  //getPosts(storyId: number): void {
-  //  this.postService.getPostsByStoryId(storyId).subscribe({
-  //    next: (data) => {
-  //      this.posts = data;
-  //    },
-  //    error: (error) => {
-  //      this.errorMessage = error; // Set error message
-  //    }
-  //  });
-  //}
-
-  //deletePost(storyId: number): void {
-  //  // Implement delete logic if needed
-  //  console.log('Delete post with ID:', storyId);
-  //}
+ 
 }
